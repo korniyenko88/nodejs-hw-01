a8909b759e077e60fc637edce39cfc14b6fc5050
unknown <you@example.com>
2024-12-03 15:59:59 +0200

    update

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,12 @@
-const generateContacts = async (number) => {};
+import * as fs from "node:fs";
+
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+
+const generateContacts = async (number) => {
+  const contactsList = Array(number).fill(0).map(createFakeContact);
+
+  console.log(contactsList);
+};
 
 generateContacts(5);
